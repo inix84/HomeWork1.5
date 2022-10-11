12918cb2c19be696fa4541ced4acbdbc1a1fa465
inix84 <inix84@mail.ru>
2022-10-11 22:55:59 +0500

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
         weight[0]=1;
         weight[1]=2;
         weight[2]=3;
-        System.out.println("1 элемент массива = " + weight[0]+" , 2 элемент массива = " + weight[1]+"  , 3 элемент массива = " + weight[2]);
+        System.out.println(weight[0]+" ,  " + weight[1]+"  ,  " + weight[2]);
 
         float [] weight2 = {1.57f, 7.654f, 9.986f};
-        System.out.println("1 элемент массива = " + weight2[0]+" , 2 элемент массива = " + weight2[1]+"  , 3 элемент массива = " + weight2[2]);
+        System.out.println( weight2[0]+" ,  " + weight2[1]+"  ,  " + weight2[2]);
 
         boolean [] weight3 = {true, true, false};
-        System.out.println("1 элемент массива = " + weight3[0]+" , 2 элемент массива = " + weight3[1]+"  , 3 элемент массива = " + weight3[2]);
+        System.out.println( weight3[0]+" , " + weight3[1]+"  ,  " + weight3[2]);
     }
 }
