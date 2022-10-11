c26b00623dbe099926e907e04e4b6f1530ff5c78
inix84 <inix84@mail.ru>
2022-10-11 23:09:30 +0500

    4 задача

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int [] weight = new int [12];
-        weight[0]=1;
-        weight[1]=2;
-        weight[2]=3;
-        System.out.println(weight[2]+" ,  " + weight[1]+"  ,  " + weight[0]);
-
-        float [] weight2 = {1.57f, 7.654f, 9.986f};
-        System.out.println( weight2[2]+" ,  " + weight2[1]+"  ,  " + weight2[0]);
-
-        boolean [] weight3 = {true, true, false};
-        System.out.println( weight3[2]+" , " + weight3[1]+"  ,  " + weight3[0]);
+        int [] ball = new int [12];
+        ball[0]=33;
+        ball[1]=66;
+        ball[2]=99;
+        System.out.println(ball[0]+" ,  " + ball[1]+"  ,  " + ball[2]);
+        for (int i = 0; i < 3; i++) {
+            if (ball[i]%2==1) {
+                ball[i]+=1;
+            }
+        }
+        System.out.println(ball[0]+" ,  " + ball[1]+"  ,  " + ball[2]);
     }
 }
