4b1752bd5ce1c324a1b95221af0a9ac40adaded7
inix84 <inix84@mail.ru>
2022-10-11 22:58:51 +0500

    3 задача

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
         weight[0]=1;
         weight[1]=2;
         weight[2]=3;
-        System.out.println(weight[0]+" ,  " + weight[1]+"  ,  " + weight[2]);
+        System.out.println(weight[2]+" ,  " + weight[1]+"  ,  " + weight[0]);
 
         float [] weight2 = {1.57f, 7.654f, 9.986f};
-        System.out.println( weight2[0]+" ,  " + weight2[1]+"  ,  " + weight2[2]);
+        System.out.println( weight2[2]+" ,  " + weight2[1]+"  ,  " + weight2[0]);
 
         boolean [] weight3 = {true, true, false};
-        System.out.println( weight3[0]+" , " + weight3[1]+"  ,  " + weight3[2]);
+        System.out.println( weight3[2]+" , " + weight3[1]+"  ,  " + weight3[0]);
     }
 }
